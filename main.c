8b20bf2738e03dcc113f7eb7bafa087da2395829
Senan O'Connor <senan.oconnor@ucdconnect.ie>
2025-03-27 16:17:20 +0000

    Print menu

--- main.c
+++ main.c
@@ -5,12 +5,19 @@
  *      Author: Senan O'Connor & Benjamin Power
  */
 
- #include <stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include "kanban_board.h"
 
 int main(void)
 {
-	
-    printf("");
+	int choice;
+
+	printf("Menu:\n\t1. Display board\n\t2. Load board from a file\n\t3. Edit List\n\t");
+	printf("4. Edit Board\n\t5. Save board to a file\n\t6. Quit\n");
+	fflush(stdout);
+
+	printf("Enter your choice (1 - 6)");
+	fflush(stdout);
+	scanf("%d", &choice);
 }
