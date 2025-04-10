69ef7bf2417d6057cd972946b773caa955955ecc
Senan O'Connor <senan.oconnor@ucdconnect.ie>
2025-04-10 08:19:15 +0000

    Update function arguments

--- Kanban_Board.h
+++ Kanban_Board.h
@@ -1,3 +1,6 @@
+#ifndef KANBAN_BOARD_H_
+#define KANBAN_BOARD_H_
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -21,14 +24,15 @@ typedef board *headPtr;
 typedef item *itemPtr;
 
 void printList(headPtr *sPtr);
-int isEmpty(headPtr *startptr);
 
 void editList(headPtr *sPtr, char list_head[MAX_LEN]);
 void editBoard(headPtr *sPtr);
 
 void editItemName(itemPtr *cPtr);
-void addItem(itemPtr *cPtr, char value[MAX_LEN]);
-void deleteItem(itemPtr *pPtr, itemPtr *cPtr);
+void addItem(headPtr *cPtr, char value[MAX_LEN]);
+void deleteItem(itemPtr *pPtr, itemPtr *cPtr, headPtr *lPtr);
 void editListName(headPtr *cPtr);
 void addList(headPtr *sPtr, char value[MAX_LEN]);
-void deleteList(headPtr *pPtr, headPtr *cPtr);
+void deleteList(headPtr *pPtr, headPtr *cPtr, headPtr *sPtr);
+
+#endif /* KANBAN_BOARD_H_ */
